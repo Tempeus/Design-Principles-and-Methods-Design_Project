b1f3a7528ab2f68fb788b8c24684eac6e1929ec6
Tempeus <nekugm@gmail.com>
2020-04-06 16:02:59 -0400

    finished implementing Rescue feature

--- src/ca/mcgill/ecse211/project/Rescue.java
+++ src/ca/mcgill/ecse211/project/Rescue.java
@@ -1,26 +1,83 @@
 package ca.mcgill.ecse211.project;
 
+import static ca.mcgill.ecse211.project.Resources.FORWARD_SPEED;
+import static ca.mcgill.ecse211.project.Resources.TILE_SIZE;
+import static ca.mcgill.ecse211.project.Resources.leftClawMotor;
+import static ca.mcgill.ecse211.project.Resources.rightClawMotor;
+import static ca.mcgill.ecse211.project.Resources.leftMotor;
+import static ca.mcgill.ecse211.project.Resources.rightMotor;
+
 /**
  * This class is used to initiate Rescue procedures when the rescue vehicle is successfully identified
  * @author Kevin
  *
  */
 public class Rescue {
+  
+  /**
+   * The locking power of the claw motors
+   */
+  private static int lockPower = 100;
+  
+  /**
+   * The claw rotating angle
+   */
+  private static int claw_angle = 90;
 
   /**
    * This method is responsible in performing the rescue of the robot.
    * The design of the robot allows it to ignore the process of attempting to find the attachment point of the rescue cart.
    * The robot is equipped with claws that is large enough and sturdy enough to capture and lock onto the cart while the robot returns to the base.
    * 
-   * When the robot detects the cart, it will reverse 5 cm and rotate 180 degrees. This will allow the claws to be aligned with the objective cart.
-   * Afterwards, the robot will slowly move forward 5 cm and the motors of each claw will rotate 90 degrees. This will capture the rescue cart.
+   * When the robot detects the cart, it will reverse a small distance and rotate 180 degrees. This will allow the claws to be aligned with the objective cart.
+   * Afterwards, the robot will slowly move forward the same small distance and the motors of each claw will rotate 90 degrees. This will capture the rescue cart.
    * Finally, the robot will lock the motors, preventing the rescue cart to break free while the robot is returning back to base. 
    */
   public static void performRescue() {
-    //go back a few steps
-    //turn 180 degrees
-    //go forward the same few steps
+    //Adjust the robot
+    adjustRobot();
+    
     //activate claws
+    activateClaws();
+    
     //lock the claws
+    lockClaws();
+  }
+  
+  /**
+   * This method is responsible in adjusting the robot.
+   * When the robot successfully identified the object as a objective cart, it must rotate itself by 180 degrees in order to have its claws facing the cart.
+   * In order to prevent collision with the cart, the robot will reverse 15 cm and will go back after the rotation phase.
+   */
+  private static void adjustRobot() {
+    //Move the robot back, to prevent collision when rotating 180 degrees
+    leftMotor.setSpeed(FORWARD_SPEED);
+    rightMotor.setSpeed(FORWARD_SPEED);
+    leftMotor.rotate(Navigation.convertDistance(-15), true);
+    rightMotor.rotate(Navigation.convertDistance(-15), false);
+    
+    //turn 180 degrees, so the claws is facing the objective cart
+    Navigation.turnByClockwise(180);
+    
+    //Move the robot forward
+    leftMotor.rotate(Navigation.convertDistance(-15), true);
+    rightMotor.rotate(Navigation.convertDistance(-15), false);
+  }
+  
+  /**
+   * The motors of the claw will rotate 90 degrees, trapping the rescue cart
+   */
+  private static void activateClaws() {
+    leftClawMotor.rotate(claw_angle);
+    rightClawMotor.rotate(claw_angle);
+  }
+  
+  /**
+   * The motors of the claws will lock, preventing the rescue cart from escaping
+   */
+  @SuppressWarnings("deprecation")
+  private static void lockClaws() {
+    leftClawMotor.lock(lockPower);
+    rightClawMotor.lock(lockPower);
   }
 }
